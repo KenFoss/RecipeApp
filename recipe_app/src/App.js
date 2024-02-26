8db29ccaef46047d0d91e73cead37912b87bd553
Ken Foss <kenfoss0715@gmail.com>
2024-02-25 23:05:58 -0500

    try to fix react-router-dom routing

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Routes, Link} from 'react-router-dom';
+import {Route, Routes, Link, BrowserRouter} from 'react-router-dom';
 import Home from './pages/Home.js';
 import Recipes from './pages/Recipes.js'
 import RecipeInfo from './pages/RecipeInfo.js';
@@ -8,12 +8,14 @@ import MealPlanner from './pages/PlannedMeals.js';
 
 function App() {
   return (
-    <Routes>
-      <Route path = '/' element={<Home/>} />
-      <Route path = '/recipes' element={<Recipes/>}/>
-      <Route path = '/recipe-info/:id' element={<RecipeInfo/>}/>
-      <Route path = '/meal-planner' element = {<MealPlanner/>}/>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/' element={<Home/>} />
+        <Route path = '/recipes' element={<Recipes/>}/>
+        <Route path = '/recipe-info/:id' element={<RecipeInfo/>}/>
+        <Route path = '/meal-planner' element = {<MealPlanner/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
